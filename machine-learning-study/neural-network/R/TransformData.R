bc6015cb99b4f41a5f7785fc3342c159301771cd
Qunqun Yu <Qunqun_Yu@bd.com>
2015-07-08 09:57:23 -0400

    Organize the data

--- machine-learning-study/neural-network/R/TransformData.R
+++ machine-learning-study/neural-network/R/TransformData.R
@@ -7,5 +7,18 @@
 library(dplyr)
 load("neural-network/Data/categoryEachDay.RData")
 
-daydf = day_time_category_df
-behavior = unique(unlist(daydf))
+allCategory <- as.character(unique(as.vector(as.matrix(day_time_category_df))))
+allCategory <- allCategory[2:13]
+
+day_time_category_df <- as.data.frame(t(as.matrix(day_time_category_df)))
+category_list <- lapply(day_time_category_df,function(x){
+ category <-  sapply(allCategory,grepl, x, ignore.case = T)
+ category_df <- as.data.frame(category)
+ category_df$na <- apply(category_df,1,function(x) 1-sum(x))
+ category_vector <- as.vector(t(as.matrix(category_df)))
+})
+
+category_matrix <- do.call(rbind,category_list)
+
+save("category_matrix", file = "neural-network/Data/categoryMatrix.RData")
+
